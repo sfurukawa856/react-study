fcea806d395fcd415dda7d21b6f0db7cc5af518f
sfurukawa <tennis856@gmail.com>
2025-05-18 07:41:23 +0900

    add components

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,19 @@
-import Image from "next/image";
 import Footer from "@/components/Footer";
 import Links from "@/components/Links";
+
+export const metadata = {
+  title: "Aboutページ",
+};
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
+              src/app/about/page.tsx
             </code>
             .
           </li>
@@ -27,7 +22,7 @@ export default function Home() {
           </li>
         </ol>
 
-      <Links />
+        <Links />
       </main>
       <Footer />
     </div>
